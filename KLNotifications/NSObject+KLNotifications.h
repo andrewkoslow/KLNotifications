77becac45d98d2c09caf93bd2c9065b9fe2c9ba5
Andrew Koslow <andrewkoslow@gmail.com>
2014-02-17 21:01:25 +0400

    Initial commit

---
+++ KLNotifications/NSObject+KLNotifications.h
@@ -0,0 +1,22 @@
+//
+//  NSObject+KLNotifications.h
+//  KLNotifications
+//
+//  Created by Andrew Koslow on 16.12.12.
+//  Copyright (c) 2012 Andrew Koslow. All rights reserved.
+//
+
+@import Foundation;
+
+
+@interface NSObject (KLNotifications)
+
+- (void)observeNotificationsNames:(NSString *)name, ... NS_REQUIRES_NIL_TERMINATION;
+- (void)observeNotificationsForObject:(id)object names:(NSString *)name, ... NS_REQUIRES_NIL_TERMINATION;
+- (void)observeWithSelector:(SEL)selector notificationsNames:(NSString *)name, ... NS_REQUIRES_NIL_TERMINATION;
+- (void)observeWithSelector:(SEL)selector notificationsForObject:(id)object names:(NSString *)name, ... NS_REQUIRES_NIL_TERMINATION;
+- (void)stopObservingNotificationsNames:(NSString *)name, ... NS_REQUIRES_NIL_TERMINATION;
+- (void)stopObservingNotificationsForObject:(id)object names:(NSString *)name, ... NS_REQUIRES_NIL_TERMINATION;
+- (void)stopObservingNotifications;
+
+@end
